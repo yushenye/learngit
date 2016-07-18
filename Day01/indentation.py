eedd215da8f8a60a42deea03c7ef65b941f75a39
yushneye <492904738@qq.com>
2016-07-18 16:43:20 +0800

    Day01-01

---
+++ Day01/indentation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def sayhi(name):
+    print 'hello, %s' %name
+    print 'dddd'
+    if name == 'Alex':
+        print 'He is so handsome....'
+        print 'lalal'
+
+sayhi('Alex')
